8b2ca5efced16e57399058fdbdb0f03f57ddbfe4
John Maina <johnmaina109@gmail.com>
2021-05-18 23:20:08 +0300

    asynchronous code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,13 +8,34 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  void getDate() async {
+
+    // simulate a network request
+    await Future.delayed(Duration(seconds: 3), () {
+      print('yoshi');
+    });
+
+    // simulate a network request to get bio of the username
+    Future.delayed(Duration(seconds: 2), () {
+      print('vega, musician & egg collector');
+    });
+
+    print('Statement');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getDate();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title: Text('Choose Location') ,
+        title: Text('Choose Location'),
         centerTitle: true,
         elevation: 0,
       ),
